0482da872d75ba5bcb1b22833519998cc464574f
chern <christopher.t.hern@gmail.com>
2021-01-11 07:40:17 -0600

    added semi-colon at end of devices.sql

--- sql/read/devices.sql
+++ sql/read/devices.sql
@@ -6,4 +6,4 @@ SELECT devices.id, devices.created_at, hostname, loopback,  hardware.vendor, har
 SELECT devices.id, devices.created_at, hostname, loopback,  hardware.vendor, hardware.model, software.version from devices
 	JOIN hardware on hardware.id = hardware_id
 	JOIN software on software.id = software_id
-     where devices.id = 5
+     where devices.id = 5;
