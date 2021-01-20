2125e3fd1b2ce313101294cac429a3c153baac95
chern <christopher.t.hern@gmail.com>
2021-01-20 17:17:25 -0600

    added email but not yet in use

---
+++ email/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/jordan-wright/email"
+)
+func main() {
+	fmt.Println("finishing building the logic")
+// 	e := email.NewEmail()
+// 	e.From = "Chris Hern Auto <test@gmail.com>"
+// 	e.To = []string{"chern@atni.com"}
+// 	// e.Cc = []string{"test_cc@example.com"}
+// 	e.Subject = "Awesome Subject"
+// 	e.Text = []byte("Text Body is, of course, supported!")
+// 	e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
+// 	e.Send("smtp.gmail.com:587", smtp.PlainAuth("", "", "", ""))
+}
